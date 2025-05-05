925a56667949e53863d43aecb8edf98024215e57
VenCz <viniciuscesarp3@gmail.com>
2025-05-04 23:07:10 -0300

    Criação do UsuarioRepository e AuthorizationService

---
+++ src/main/java/br/com/fiap/contatos/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.com.fiap.contatos.repository;
+
+import br.com.fiap.contatos.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByEmail(String email);
+
+}
